8b2f3edb131d92400c239fdef5bec8883a2bbd18
Uniaut <kunwoo0927@gmail.com>
2022-01-12 16:14:57 +0900

    modal feature

--- DdayChat/App.js
+++ DdayChat/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {
   Image,
+  ImageBackground,
   View,
   ScrollView,
   StyleSheet,
@@ -12,13 +13,85 @@ import {
 import Setting from './Setting.js';
 
 
+
+const settingIconUri = 'https://iconmonstr.com/wp-content/g/gd/makefg.php?i=../assets/preview/2012/png/iconmonstr-gear-2.png';
+
+export default class App extends React.Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      date: new Date(2027, 0, 3),
+      title: '박사 취득',
+      log: '졸업?\n그 논문으로?\n졸업?\n그 논문으로?\n졸업?\n그 논문으로?\n',
+      settingModal: false,
+    }
+  }
+
+  toggleSettingModal() {
+    this.setState({settingModal: !this.state.settingModal})
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <ImageBackground
+          style={{width: '100%', height: '100%'}}
+          source={require('./resources/bonobono.jpg')}
+        >
+          <View style={styles.settingView}>
+            <TouchableOpacity 
+              style={styles.settingIconTouch}
+              onPress={() => {this.toggleSettingModal()}}
+            >
+              <Image
+                source={{uri:settingIconUri}}
+                style={styles.settingIcon}
+                />
+            </TouchableOpacity>
+          </View>
+          <View style={styles.ddayView}>
+            <Text style={styles.ddayDateText}>
+              {this.state.date.toDateString()}
+            </Text>
+            <Text style={styles.ddayEventText}>
+              {this.state.title}까지
+            </Text>
+            <Text style={styles.ddayDayText}>
+              {}일
+            </Text>
+          </View>
+          <View style={styles.chatView}>
+            <View style={styles.chatLogView}>
+              <ScrollView>
+                <Text style={styles.chatLogText}>
+                  {this.state.log}
+                </Text>
+              </ScrollView>
+            </View>
+            <View style={styles.chatInputBarView}>
+              <TextInput style={styles.chatInputView}/>
+              <TouchableOpacity style={styles.chatButtonView}>
+                <Text>
+                  논문 제출
+                </Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+          
+          {this.state.settingModal ? <Setting/> : <></>}
+        
+        </ImageBackground>
+      </View>
+    )
+  }
+};
+
 const styles = StyleSheet.create({
   container: {
     flex:1,
   },
   settingView: {
-    backgroundColor:'lightgray',
-    flex:1,
+    height:70,
     justifyContent:'center',
     alignItems:'flex-end',
   },
@@ -31,10 +104,12 @@ const styles = StyleSheet.create({
     width:50,
   },
   ddayView: {
-    backgroundColor:'lightyellow',
-    flex:3,
+    backgroundColor:'rgba(255, 255, 100, 0.4)',
+    height:200,
     justifyContent:'center',
-    paddingHorizontal:80,
+    paddingHorizontal:20,
+    margin:30,
+    borderRadius:30,
   },
   ddayDateText: {
     fontSize:20,
@@ -50,7 +125,7 @@ const styles = StyleSheet.create({
     fontStyle:'normal',
   },
   chatView: {
-    backgroundColor:'lightgreen',
+    backgroundColor:'rgba(100, 255, 100, 0.4)',
     flex:6,
     justifyContent:'center',
     alignItems:'center',
@@ -61,8 +136,10 @@ const styles = StyleSheet.create({
     borderWidth:1,
     borderColor:'steelblue',
     borderRadius:10,
-    height:'80%',
     width:'90%',
+  },
+  chatLogText: {
+    fontSize: 50,
   },
   chatInputBarView: {
     flexDirection:'row',
@@ -92,57 +169,3 @@ const styles = StyleSheet.create({
     justifyContent:'center'
   },
 });
-
-const settingIconUri = 'https://iconmonstr.com/wp-content/g/gd/makefg.php?i=../assets/preview/2012/png/iconmonstr-gear-2.png';
-
-export default class App extends React.Component {
-  constructor (props) {
-    super(props);
-    this.state = {
-      date: new Date(2027, 0, 3),
-      title: 'PH.D'
-    }
-  }
-  render() {
-    return (
-      <View style={styles.container}>
-        <View style={styles.settingView}>
-          <TouchableOpacity style={styles.settingIconTouch}>
-            <Image
-              source={{uri:settingIconUri}}
-              style={styles.settingIcon}
-            />
-          </TouchableOpacity>
-        </View>
-        <View style={styles.ddayView}>
-          <Text style={styles.ddayDateText}>
-            {this.state.date.toDateString()}
-          </Text>
-          <Text style={styles.ddayEventText}>
-            {this.state.title}까지
-          </Text>
-          <Text style={styles.ddayDayText}>
-            {}일
-          </Text>
-        </View>
-        <View style={styles.chatView}>
-          <View style={styles.chatLogView}>
-            <ScrollView>
-              <Text>
-                {'히히 못가'}
-              </Text>
-            </ScrollView>
-          </View>
-          <View style={styles.chatInputBarView}>
-            <TextInput style={styles.chatInputView}/>
-            <TouchableOpacity style={styles.chatButtonView}>
-              <Text>
-                논문 제출
-              </Text>
-            </TouchableOpacity>
-          </View>
-        </View>
-      </View>
-    )
-  }
-};
--- DdayChat/Setting.js
+++ DdayChat/Setting.js
@@ -1,23 +1,92 @@
 import React from 'react';
 import {
-    View,
-    StyleSheet,
-    Text,
-    TextInput,
-    TouchableOpacity
+  View,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity
 } from 'react-native';
+import DatePicker from 'react-native-date-picker';
 
 
 export default class Setting extends React.Component {
-    render() {
-        return (
-            <View style={styles.container}>
-                <Text>설정창</Text>
-            </View>
-        )
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: '',
+      date: new Date(),
+    };
+  }
+  render() {
+    return (
+      <View style={styles.container}>
+        <TouchableOpacity style={styles.background}/>
+        <View style={styles.modal}>
+          <Text style={styles.titleText}>설정</Text>
+          <TextInput
+            onChangeText={(changedText)=>{this.setState({title: changedText})}}
+            style={styles.titleInput}
+            placeholder={'이벤트 내용을 입력해주세요'}
+          />
+          <DatePicker
+            date={this.state.date}
+            mode="date"
+          />
+          <TouchableOpacity>
+            <Text style={styles.doneText}>
+              변경
+            </Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    )
+  }
 }
 
 const styles = StyleSheet.create({
+  container: {
+    position: 'absolute',
+    height: '100%',
+    width: '100%',
+    backgroundColor: 'transparent',
+  },
+  background: {
+    position: 'absolute',
+    height:'100%',
+    width:'100%',
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+  },
+  titleInput: {
+    backgroundColor: 'white',
+    marginBottom: 20,
+    width: '75%',
+    height: 40,
+    borderBottomWidth: 1,
+    borderColor: 'blue',
+  },  
+  ddayInput: {
+    backgroundColor: 'white',
+    marginBottom: 20,
+    width: '75%',
+    height: 40,
+    borderBottomWidth: 1,
+    borderBottomColor: 'blue'
+  },
+  modal: {
+    marginHorizontal: 20,
+    borderRadius: 10,
+    alignItems: 'center',
+    marginTop: '50%',
+    backgroundColor: 'white',
 
+  },
+  doneText: {
+    color: 'rgb(1, 100, 255)',
+    fontSize: 15,
+    margin: 10,
+  },
+  titleText: {
+    fontSize: 18,
+    margin: 10,
+  }
 });
